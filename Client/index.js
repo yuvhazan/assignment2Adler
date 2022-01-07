3c95db62bc8396ae9d1281f6abe4a7de15556923
OmriStudies <baroz.omri@gmail.com>
2022-01-07 16:10:34 +0200

    DID NOT CHANGE THE LOGIC
    only abstracted a bit the code and split to small functions instead of big function

--- Client/index.js
+++ Client/index.js
@@ -34,11 +34,16 @@ let numOfReady = 0
 
 let clientsConnectsToMe = clientList.filter((client) => client.id < myId).length
 let maxId = myId
-clientList.forEach(client => {
-    if (client.id > maxId){
-        maxId = client.id
-    }
-})
+// TODO: I added this one because it can help us with the condition when we
+//  need to know if we are the minimum id
+let minId = myId
+
+const checkMinMaxId = (client) => {
+    maxId = client.id ? client.id > maxId : maxId
+    minId = client.id ? client.id < minId : minId
+}
+
+clientList.forEach(checkMinMaxId)
 
 const imMax = maxId === myId
 let maxSocket = null
@@ -50,8 +55,84 @@ const debug = (msg) => {
     console.log(`DEBUG: ${msg}`)
 }
 
+FgRed = "\x1b[31m"
+
+const error = (msg) => {
+    console.log(FgRed + `ERROR: ${msg}`)
+}
+
 const jsonToBuffer = (json) => {
     return Buffer.from(JSON.stringify(json) + "\n")
+}
+
+const writeToData = (socket, data) => {
+    socket.write(data)
+}
+
+const writeStart = (socket) => {
+    socket.write('start')
+}
+
+const writeReady = (socket) => {
+    socket.write(`ready ${myId}`)
+}
+
+const writeGoodbye = (socket) => {
+    socket.write(`goodbye ${myId}`)
+}
+
+
+const applyOperation = (operation) => {
+    const action = operation !== undefined ? operation.split(' ') : ''
+    const op = action[0]
+    debug('in apply operation')
+    console.log(operation)
+
+    switch (op) {
+        case "insert": {
+            const char = action[1]
+            const index = action.length === 3 ? parseInt(action[2]) : stringReplica.length
+            const prefix = stringReplica.substr(0, index)
+            const suffix = stringReplica.substr(index)
+            updateStringReplica(prefix + char + suffix)
+            return
+        }
+        case "delete": {
+            const index = parseInt(action[1])
+            const prefix = stringReplica.substr(0, index)
+            const suffix = stringReplica.substr(index + 1)
+            updateStringReplica(prefix + suffix)
+            return
+        }
+        default: {
+            console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx oi vavoi XXXXXXXXXXXXXXXXXX')
+
+        }
+    }
+}
+
+const eventLoop = async () => {
+    const handleOperation = async (operation) => {
+        console.log(operation)
+        applyOperation(operation)
+        finishedOperations++
+        if(finishedOperations === numOfOperations){
+            log(`Client ${myId} finished his local string modifications`)
+        }
+        myTimeStamp++
+        const timeStamp = myTimeStamp
+        const id = myId
+        operationHistory.push({operation: operation, timeStamp: timeStamp, updatedString: stringReplica, id: id})
+        const data = {operation, timeStamp, id}
+        socketList.forEach(socket => writeToData(socket, jsonToBuffer(data)))
+    }
+
+    let finishedOperations = 0
+    console.log(operationsList)
+    operationsList.forEach(operation => {
+        console.log(operation)
+        setTimeout(() => handleOperation(operation), Math.random() * 10000)
+    })
 }
 
 const mergeAlgorithm = (action, timeStamp, id) => {
@@ -132,157 +213,212 @@ const handleData = (socket, data) => {
     data.map(_handleData)
 }
 
+const handleEnd = () => {
+    console.log(`Closing connection with client ${socket}`)
+}
+
+const handleError = (err, msg=null) => {
+    if (msg){
+        error(msg)
+    }
+    error(e.stack)
+    error(e.name)
+    error(e.message)
+}
+
+const decreaseNumOfSockets = () => {
+    numOfSockets--
+}
+
+const decreaseClientsConnection = () => {
+    clientsConnectsToMe--
+}
+
+const checkLastClientConnect = () => {
+    if (clientsConnectsToMe === 0){
+        if(imMax){
+            // TODO: check what to do if it is the last connection of the max ID
+        }else{
+            debug(`${myId} is ready`)
+            writeReady(maxSocket)
+        }
+    }
+}
+
+const addSocketToList = (socket) => {
+    socketList.push(socket)
+}
+
+const increaseNumOfSockets = () => {
+    numOfSockets++
+}
+
+const increaseNumOfReady = () => {
+    numOfReady++
+}
+
+const handleServerConnection = (socket) => {
+    decreaseClientsConnection();
+
+    checkLastClientConnect()
+
+    increaseNumOfSockets()
+
+    addSocketToList(socket)
+
+    socket.on('data', (data) => {
+        handleData(socket, data)
+    })
+
+    socket.on('start',start)
+
+    socket.on('end', handleEnd)
+
+    socket.on('error', handleError);
+}
+
 const createServer = (server) => {
     server.listen(port, () => console.log(`hello world on port ${port}`))
-
-    server.on('connection', (socket) => {
-        clientsConnectsToMe--;
-        if (clientsConnectsToMe === 0){
-            if(imMax){
-
-            }else{
-                debug(`${myId} is ready`)
-                let id = myId
-                maxSocket.write(`ready ${myId}`)
-            }
-        }
-        numOfSockets++;
-        socketList.push(socket)
-        // operationHistory.forEach(({operation, timeStamp, stringReplica, id}) => {
-        //     if(id === myId){
-        //         const data = {operation, timeStamp, id}
-        //         socket.write(Buffer.from(JSON.stringify(data) + "\n"))
-        //     }
-        // })
-        socket.on('data', (data) => {
-            handleData(socket, data)
-        })
-
-        socket.on('start', () =>{
-            start()
-        })
-
-        socket.on('end', () => {
-            console.log(`Closing connection with the client ${socket}`)
-        })
-
-        socket.on('error', (err) => {
-            console.log(`Error: ${err}`);
-            console.log(err.stack)
-        });
-    })
+    server.on('connection', handleServerConnection)
 }
 
-const applyOperation = (operation) => {
-    const action = operation !== undefined ? operation.split(' ') : ''
-    const op = action[0]
-    debug('in apply operation')
-    console.log(operation)
 
-    switch (op) {
-        case "insert": {
-            const char = action[1]
-            const index = action.length === 3 ? parseInt(action[2]) : stringReplica.length
-            const prefix = stringReplica.substr(0, index)
-            const suffix = stringReplica.substr(index)
-            updateStringReplica(prefix + char + suffix)
-            return
-        }
-        case "delete": {
-            const index = parseInt(action[1])
-            const prefix = stringReplica.substr(0, index)
-            const suffix = stringReplica.substr(index + 1)
-            updateStringReplica(prefix + suffix)
-            return
-        }
-        default: {
-            console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx oi vavoi XXXXXXXXXXXXXXXXXX')
-
-        }
-    }
-}
-
-const eventLoop = async () => {
-    const handleOperation = async (operation) => {
-        console.log(operation)
-        applyOperation(operation)
-        finishedOperations++
-        if(finishedOperations === numOfOperations){
-            log(`Client ${myId} finished his local string modifications`)
-        }
-        myTimeStamp++
-        const timeStamp = myTimeStamp
-        const id = myId
-        operationHistory.push({operation: operation, timeStamp: timeStamp, updatedString: stringReplica, id: id})
-        const data = {operation, timeStamp, id}
-        // if(tenOperation && finishedOperations%10 === 0) {
-            socketList.forEach(socket => socket.write(jsonToBuffer(data)))
-        // }
-    }
-
-    let finishedOperations = 0
-    console.log(operationsList)
-    operationsList.forEach(operation => {
-        console.log(operation)
-        setTimeout(() => handleOperation(operation), Math.random() * 10000)
-    })
-}
-
+/**
+ * handle the 'ready' event.
+ * only the highest id client should get this event.
+ * increases the number of ready
+ * checks if all clients are ready, if they are sending start to all the clients, and starting myself
+ */
 const handleReady = () => {
-    console.log(`Client ${myId} is ready`)
-    numOfReady++
+    debug(`Client ${myId} is ready`)
+    increaseNumOfReady()
+
     if(numOfReady === clientList.length){
-        socketList.forEach(socket => socket.write('start'))
+        socketList.forEach(writeStart)
         start()
     }
 }
 
+/**
+ * decreases the num of sockets that the client is connected to,
+ * and if it was the last one it kills the connection with all the sockets and ends
+ */
+//TODO: check if there is a problem that I'm ending the connection only after all of my connections ended
 const handleGoodbye = () => {
-    numOfSockets--
+    decreaseNumOfSockets()
+
     if (numOfSockets === 0) {
         socketList.forEach(socket => socket.end())
-        console.log(`final string: ${stringReplica}`)
+        log(`final string: ${stringReplica}`)
         log(`Client ${myId} is exiting`)
-        console.log("Operation history:")
-        operationHistory.forEach(history => console.log(history))
+        //TODO: remove the print after finish debugging
         process.exit(0)
     }
 }
 
-
+/**
+ * sends goodbye to all of the sockets.
+ * activates handleGoodbye to decrease the initial value
+ * @returns {Promise<void>}
+ */
 const goodbye = async () => {
-    socketList.forEach(socket => {
-        socket.write(`goodbye ${myId}`)
-    })
+    socketList.forEach(writeGoodbye)
     handleGoodbye()
 }
 
+/**
+ * creates a connection to the server using the net module
+ * @param port the port to connect to
+ * @param host the host to connect to
+ * @param callback the callback to perform after connection successful
+ * @returns {Socket} the socket of the server that was connected to at host:port
+ */
+const createConnection = (port, host, callback) => {
+    return net.createConnection(port, host, callback)
+}
+
+/**
+ * handles the error event when recieving from clients
+ * @param err
+ */
+const handleClientError = (err) => {
+    handleError(err, `got error on client id ${id} port ${port}`)
+}
+
+/**
+ * returns if the id is the maxId and that the client is the one with max Id
+ * @param id id to check
+ * @returns {boolean} true if id is the maxId and the client is the maxId
+ */
+//TODO: check if there is an option that the imMax will be true when calling it from connectToServer
+const checkMaxId = (id) => !imMax && maxId === id
+
+
+/**
+ * updates the maxSocket to be the socket that is given.
+ * This function assumes that the socket it is given is the socket of the client
+ * with the highest id
+ * also sends ready if it is the client with the lowest id
+ * @param socket the socket to be max socket
+ */
+const updateMaxSocket = (socket) => {
+    maxSocket = socket
+    // TODO: what is this if? checks if im the lowest one?
+    if(clientsConnectsToMe === 0) {
+        writeReady(socket)
+        debug(`${myId} ready`)
+    }
+}
+
+/**
+ * connects to the server of client id ant host:port.
+ *
+ * @param id the id of the client to connect
+ * @param host the host of the client
+ * @param port the port of the client
+ */
+const connectToServer = (id, host, port) => {
+
+    let socket = undefined;
+
+    socket = createConnection(port, host, () => console.log(`Connected to client ${host}:${port}`))
+
+    socket.on('data', (data) => {
+        handleData(socket, data)
+    })
+
+    socket.on('error', handleClientError)
+
+    socket.on('end', () => console.log('closing connection with client ', id))
+
+    increaseNumOfSockets()
+
+    addSocketToList(socket)
+
+    const isMaxId = checkMaxId(id)
+
+    if(isMaxId){
+        updateMaxSocket(socket)
+    }
+}
+
+/**
+ * connect to all the clients that are in the client list
+ * @param clientList the list of clients to connect
+ */
 const connectClients = (clientList) => {
     clientList.forEach(({id, host, port}) => {
+        // connect only to the servers of the Id's bigger than me
         if (myId < id) {
-            let socket = undefined;
-            socket = net.createConnection(port, host, () => console.log(`Connected to client ${host}:${port}`))
-            socket.on('data', (data) => {
-                handleData(socket, data)
-            })
-            socket.on('error', (err) => {
-                console.log(`got error on client id ${id} port ${port} error: ${err.toString()}`)
-            })
-            socket.on('end', () => console.log('closing connection with client ', id))
-            numOfSockets++;
-            socketList.push(socket)
-            if(!imMax && maxId === id){
-                maxSocket = socket
-                if(clientsConnectsToMe === 0) {
-                    let id = myId
-                    socket.write(`ready ${myId}`)
-                    debug(`${myId} ready`)
-                }
-            }
+            connectToServer(id, host, port)
         }
     })
 }
 
+
+/**
+ * main function, starts the event loop after 10 seconds and the goodbye after 30 seconds
+ */
 const start = () => {
     setTimeout(eventLoop, 10000)
     setTimeout(goodbye, 30000)
